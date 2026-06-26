59c5741ededb3bbbe66d878784ae2c2aead73bc1
FadyAshraf-dev <fady_ashraf2007@hotmail.com>
2026-06-26 20:28:18 +0300

    refactor(Role): making a constant containing class to use with Gatekeeper

---
+++ classes/Role.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+final class Role
+{
+    public const CUSTOMER = 1;
+    public const VENDOR = 2;
+    public const ADMIN = 3;
+
+    private function __construct()
+    {
+    }
+}
