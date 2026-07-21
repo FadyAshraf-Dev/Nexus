232e1cb35a724762e459bade7fb7f84abfdd821a
FadyAshraf-dev <fady_ashraf2007@hotmail.com>
2026-07-21 03:39:27 +0300

    making a better header.php

--- public/includes/header.php
+++ public/includes/header.php
@@ -1,19 +1,61 @@
-  <header class="header">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-3 col-md-3">
-          <div class="header__logo header__brand">
-            <a href="index.html">
-              <span class="header__brand-title">NEXUS</span>
-              <span class="header__brand-subtitle">Fashion Marketplace</span>
-            </a>
-          </div>
-        </div>
-        <div class="col-lg-6 col-md-6">
-        </div>
-        <div class="col-lg-3 col-md-3">
-        </div>
+<header class="header">
+  <div class="container">
+    <div class="header__content">
+
+      <div class="header__logo">
+        <a href="index.html">
+          <span class="header__brand-title">NEXUS</span>
+          <span class="header__brand-subtitle">Fashion Marketplace</span>
+        </a>
       </div>
-      <div class="canvas__open"><i class="fa fa-bars"></i></div>
+
+        <form class="header__search">
+
+          <input type="text" id="global-search" placeholder="Search products...">
+
+          <button type="submit">
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+              stroke="currentColor" stroke-width="2">
+
+              <circle cx="11" cy="11" r="7"></circle>
+
+              <line x1="21" y1="21" x2="16.65" y2="16.65">
+              </line>
+
+            </svg>
+
+          </button>
+
+        </form>
+
+      <div class="header__actions">
+
+        <a href="/login.php">
+
+          <span>Hello, Sign in</span>
+
+          <strong>Account</strong>
+
+        </a>
+
+        <a href="#">
+
+          <span>Returns</span>
+
+          <strong>& Orders</strong>
+
+        </a>
+
+        <a href="/shopping-cart.php" class="header__cart">
+
+          <img src="img/icon/cart.png" alt="">
+
+          <span>Cart</span>
+
+        </a>
+      </div>
+
     </div>
-  </header>
+  </div>
+</header>
--- public/shop.php
+++ public/shop.php
@@ -33,12 +33,6 @@ require_once dirname(__DIR__, 1) . '/bootstrap/bootstrap.php';
             <div class="row">
                 <div class="col-lg-3">
                     <div class="shop__sidebar">
-                        <div class="shop__sidebar__search">
-                            <form action="#">
-                                <input type="text" placeholder="Search...">
-                                <button type="submit"><span class="icon_search"></span></button>
-                            </form>
-                        </div>
                         <div class="shop__sidebar__accordion">
                             <div class="accordion" id="accordionExample">
                                 <div class="card">
@@ -63,37 +57,22 @@ require_once dirname(__DIR__, 1) . '/bootstrap/bootstrap.php';
                                         </div>
                                     </div>
                                 </div>
-                                <div class="card-heading">
-                                    <a data-toggle="collapse" data-target="#collapseTwo">Branding</a>
-                                </div>
-                                <div id="collapseTwo" class="collapse show" data-parent="#accordionExample">
-                                    <div class="card-body">
-                                        <div class="shop__sidebar__brand">
-                                            <ul>
-                                                <li><a href="#">Louis Vuitton</a></li>
-                                                <li><a href="#">Chanel</a></li>
-                                                <li><a href="#">Hermes</a></li>
-                                                <li><a href="#">Gucci</a></li>
-                                            </ul>
-                                        </div>
+                                <div class="card">
+                                    <div class="card-heading">
+                                        <a data-toggle="collapse" data-target="#collapseThree">Filter Price</a>
                                     </div>
-                                </div>
-                            </div> -->
-                            <div class="card">
-                                <div class="card-heading">
-                                    <a data-toggle="collapse" data-target="#collapseThree">Filter Price</a>
-                                </div>
-                                <div id="collapseThree" class="collapse show" data-parent="#accordionExample">
-                                    <div class="card-body">
-                                        <div class="shop__sidebar__price">
-                                            <ul>
-                                                <li><a href="#">$0.00 - $50.00</a></li>
-                                                <li><a href="#">$50.00 - $100.00</a></li>
-                                                <li><a href="#">$100.00 - $150.00</a></li>
-                                                <li><a href="#">$150.00 - $200.00</a></li>
-                                                <li><a href="#">$200.00 - $250.00</a></li>
-                                                <li><a href="#">250.00+</a></li>
-                                            </ul>
+                                    <div id="collapseThree" class="collapse show" data-parent="#accordionExample">
+                                        <div class="card-body">
+                                            <div class="shop__sidebar__price">
+                                                <ul>
+                                                    <li><a href="#">$0.00 - $50.00</a></li>
+                                                    <li><a href="#">$50.00 - $100.00</a></li>
+                                                    <li><a href="#">$100.00 - $150.00</a></li>
+                                                    <li><a href="#">$150.00 - $200.00</a></li>
+                                                    <li><a href="#">$200.00 - $250.00</a></li>
+                                                    <li><a href="#">250.00+</a></li>
+                                                </ul>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -101,6 +80,7 @@ require_once dirname(__DIR__, 1) . '/bootstrap/bootstrap.php';
                         </div>
                     </div>
                 </div>
+
                 <div class="col-lg-9">
                     <div class="shop__product__option">
                         <div class="row">
@@ -142,9 +122,6 @@ require_once dirname(__DIR__, 1) . '/bootstrap/bootstrap.php';
 
     <!-- Footer Section End -->
 
-    <!-- Search Begin -->
-    <?php include 'includes/search.php'; ?>
-    <!-- Search End -->
 
     <!-- Js Plugins -->
     <?php include 'includes/scripts.php'; ?>
@@ -153,7 +130,6 @@ require_once dirname(__DIR__, 1) . '/bootstrap/bootstrap.php';
 
         new Shop().init();
     </script>
-    <div id="shop-pagination" class="product__pagination"></div>
 </body>
 
 </html>
